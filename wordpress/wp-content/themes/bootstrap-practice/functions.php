8acf6c619ef5e0257730e16ad739acd293e8978f
Thom Wise <thomwise@admins-MacBook-Pro.local>
2019-12-18 17:00:56 -0800

    Added navbar to header and more bootstrap features to index file

--- wordpress/wp-content/themes/bootstrap-practice/functions.php
+++ wordpress/wp-content/themes/bootstrap-practice/functions.php
@@ -8,7 +8,7 @@ function themebs_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles');
 
 function themebs_enqueue_scripts() {
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.bundle.min.js', array( 'jquery' ) );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', array( 'jquery' ) );
 }
 add_action( 'wp_enqueue_scripts', 'themebs_enqueue_scripts');
 ?>
--- wordpress/wp-content/themes/bootstrap-practice/header.php
+++ wordpress/wp-content/themes/bootstrap-practice/header.php
@@ -13,7 +13,43 @@
 
     <meta name="viewport" content="width=device-width">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="stylesheet" href="style.css">
 
     <?php wp_head(); ?>
-    <h5>I am the header</h5>
 </head>
+<body>
+    <header class="container-fluid d-flex justify-content-center">
+            <h1>You're going to love the most amazing website ever!</h1>
+    </header>
+    <nav class="container navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <div class="d-flex justify-content-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown Items
+                    </a>
+                    <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item text-white" href="#">Tickles</a>
+                        <a class="dropdown-item text-white" href="#">Rainbows</a>
+                        <a class="dropdown-item text-white" href="#">Unicorns</a>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    </div>
+    </nav>
+</body>
